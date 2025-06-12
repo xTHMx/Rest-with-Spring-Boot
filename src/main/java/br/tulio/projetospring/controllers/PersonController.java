ecc9bc2a0b442df592b16f8fcae96efa40b18ba3
xTHMx <122451327+xTHMx@users.noreply.github.com>
2025-06-11 23:09:14 -0300

    Refactoring person controller functions

--- src/main/java/br/tulio/projetospring/controllers/PersonController.java
+++ src/main/java/br/tulio/projetospring/controllers/PersonController.java
@@ -4,6 +4,7 @@ import br.tulio.projetospring.models.Person;
 import br.tulio.projetospring.request.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,30 +17,32 @@ public class PersonController {
     private PersonServices services;
 
     //quando não houver valor no mapping, é o metodo default (/person) que sera chamado
-    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE,
+    //@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE,          *Não se usa mais (Legado)
+    //       produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public Person create(@RequestBody Person person) { //@RequestBody faz retornar o body do request
         return services.create(person);
     }
 
-    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person update(@RequestBody Person person) { //requestbody faz retornar o body do request
-        return services.update(person);
-    }
-
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public void delete(@PathVariable("id") Long id) { //requestbody faz retornar o body do request
-        services.delete(id);
-    }
-
     //chama um endpoint com metodo get e retorna um json
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findByID(@PathVariable("id") Long id) {
         return services.findByID(id);
     }
 
-    @RequestMapping(value = "/all", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping( consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person) { //requestbody faz retornar o body do request
+        return services.update(person);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) { //requestbody faz retornar o body do request
+        services.delete(id);
+        return ResponseEntity.noContent().build(); //retorna o status correto (204)
+    }
+
+    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Person> findAll() {
         return services.findAll();
     }
